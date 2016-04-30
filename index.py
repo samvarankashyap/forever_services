48b47064d54ed9d18fca39de3b7e48f0a17a1cc5
samvarankashyap <samvaran2best@gmail.com>
2016-04-30 01:08:24 -0400

    added start, stop , list services and service_scripts folder

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import Flask, request, send_from_directory
 app = Flask(__name__,static_url_path='/static')
+from subprocess import Popen, PIPE
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
@@ -9,23 +10,52 @@ def hello(name=None):
     return render_template('hello.html', name=name)
 
 
-@app.route('/js/<path:path>')
-def send_js(path):
-    return send_from_directory('./static/js', path)
-
-@app.route('/css/<path:path>')
-def send_css(path):
-    return send_from_directory('./static/css', path)
-
 @app.route('/')
 def index():
     return 'Index Page'
 
+@app.route('/list_forever_services')
+def list_forever_services():
+    cmd = get_command("list")
+    output = run_command(cmd)
+    return output
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
+@app.route('/start_service', methods=['POST'])
+def start_service():
+    service_name =  request.form['service_name'];
+    script_name =  request.form['script_name'];
+    cmd = get_command("start")
+    output = run_command(cmd, script_name)
+    return output
+
+@app.route('/stop_service', methods=['POST'])
+def stop_service():
+    service_name =  request.form['service_name'];
+    cmd = get_command("stop")
+    cmd.append(service_name)
+    output = run_command(cmd) 
+    return output
+
+def run_command(command, script_name = None):
+    if script_name == None:
+        p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    else:
+        command.append(str("./service_scripts/"+script_name))
+        p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        
+    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+    rc = p.returncode
+    return output
+   
+
+def get_command(type_of_command):
+    command_list = {
+        "list": ["forever", "list"],
+        "stop": ["forever", "stop"],
+        "start": ["forever", "start", "-c","python"]
+    }
+    return command_list[type_of_command]
+
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
---
+++ service_scripts/hello.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import time
+
+f = open("h.txt","a")
+f.write("this is hello\n")
+f.close()
+time.sleep(5)
